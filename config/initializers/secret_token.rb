017a8bb949c32f38821736ad979b4a21ae5af4c4
Aladine <alazaiane@yahoo.fr>
2014-03-10 18:14:22 +0100

    deploiement heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EnssApp::Application.config.secret_key_base = '220e01b570ad01ab00cea6e70bef68f785807bc4f517370454ae05e16fc2827ba0c8d7e520ad8d43b5e45b8c4c59a84f7148a5ff1313370147bd5736ade93cf8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EnssApp::Application.config.session_store :cookie_store, key: '_enss_app_session'
